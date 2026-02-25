4e8ed67c99a468e2a89fdb4a2d733b275cb8f269
Roman <davidovshag@gmail.com>
2026-02-25 10:14:14 +0200

    MakeCreateCar

---
+++ vite-project/src/types/ICreateCar.ts
@@ -0,0 +1,13 @@
+
+// будемо проводити на основі даної моделі
+// створення авто
+export interface ICreateCar {
+    id: number;
+    model: string;
+    mark: string;
+    description: string;
+    year: number;
+    color: string;
+    image: string;
+    price: number;
+}
